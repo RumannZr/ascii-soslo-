a4e044d94a4e2671bdc201bfe97611682148d207
RumannZr <enot22898@gmail.com>
2025-11-03 02:21:29 +0300

    Fixed A lot of shit code + added braille pattern

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PIl import Image
+from PIL import Image
 from pathlib import Path
 
 
@@ -7,30 +7,42 @@ IMAGE_PATH = Path(input("Введите путь к файлу: "))
 BRAILLE_CHARS = ""
 
 
-
-def ascii_convert(IMAGE_PATH, ASCII_CHARS):
-
+def braille_convert(IMAGE_PATH, BRAILLE_CHARS):
     try:
         with Image.open(IMAGE_PATH) as img:
+            width, height = img.size
+            aspect_ratio = 0.5
+            output_width = width * 2
+            output_height = (output_width * aspect_ratio) * 4
+            img_prep = img.convert("L").resize(output_width, output_height)
 
-        width, height = img.size()
-        aspect_ratio = 0.5
-        output_width = 100
-        output_height = output_width * aspect_ratio
-        img_prep = img.convert('L').resize(output_width, output_height)
+            output = []
 
-        output = []
+    except FileNotFoundError:
+        print(f"File was not found")
+    except Exception as e:
+        print(f"An error was accured: {e}")
 
-        for y in range(output_height):
 
-            for x in range(output_width):
+def ascii_convert(IMAGE_PATH, ASCII_CHARS):
+    try:
+        with Image.open(IMAGE_PATH) as img:
+            width, height = img.size
+            aspect_ratio = 0.5
+            output_width = 100
+            output_height = output_width * aspect_ratio
+            img_prep = img.convert("L").resize(output_width, output_height)
 
-                map_index = img.getpixel(x,y) * (len(ASCII_CHARS) - 1) // 255
-                output_string.append(ASCII_CHARS[map_index])
+            output = []
 
-            output.append("\n") 
+            for y in range(output_height):
+                for x in range(output_width):
+                    map_index = img.getpixel(x, y) * (len(ASCII_CHARS) - 1) // 255
+                    output.append(ASCII_CHARS[map_index])
 
-    except FileNotFound:
+                output.append("\n")
+
+    except FileNotFoundError:
         print(f"File was not found")
     except Exception as e:
         print(f"An error was accured: {e}")
